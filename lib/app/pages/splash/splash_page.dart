f557a7807893e911e3f59648044b176e0a105be9
Henrick <ricknogueira1231@gmail.com>
2024-01-21 16:09:49 -0300

    Configurando o projeto e preparando o tema

---
+++ lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Teste"),
+          ),
+          TextFormField()
+        ],
+      ),
+    );
+  }
+}
